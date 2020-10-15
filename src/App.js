b1a65e1d83f8b180c2a17070d7e443a102e299b2
ofkarakus <omerfarukkarakus01@gmail.com>
2020-10-15 21:11:16 +0100

    feat: ready for presentation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,154 @@
 import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Dimensions,
+  FlatList,
+} from 'react-native';
+
+import {ToDoCard} from './components';
 
 const App = () => {
-  
-  
+  const [counter, setCounter] = useState(0);
+  const [todo, setTodo] = useState('');
+  const [todoList, setTodoList] = useState([]);
+  const [todoId, setId] = useState(0);
+
+  // const checkDuplicate = () => {
+  //   todoList.forEach((item) => {
+  //     if (item.text == todo) {
+  //       return alert('abc');
+  //     }
+  //   });
+  //   addTodo();
+  // };
+
+  const addTodo = () => {
+    setTodoList([{text: todo, id: todoId}, ...todoList]);
+  };
+  const increaseCounter = () => {
+    setCounter(counter + 1);
+  };
+  const decreaseCounter = () => {
+    setCounter(counter - 1);
+  };
+  const getInput = (text) => {
+    setTodo(text);
+  };
+
+  const deleteTodo = (currentTodosId) =>
+    setTodoList(todoList.filter((todo) => todo.id !== currentTodosId));
+
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.heading}>TODO</Text>
+        <Text style={styles.counter}>{counter}</Text>
+      </View>
 
+      <View style={styles.main}>
+        <FlatList
+          keyExtractor={(item) => item.id.toString()}
+          data={todoList}
+          renderItem={({item}) => (
+            <ToDoCard
+              content={item}
+              deleteTodo={deleteTodo}
+              decreaseCounter={decreaseCounter}
+            />
+          )}
+        />
+      </View>
 
-    </SafeAreaView>
-  )
-}
+      <View style={styles.footer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Type here to add your ToDo!"
+          onChangeText={getInput}
+          onSubmitEditing={(event) => {
+            setTodo(event.nativeEvent.text);
+            increaseCounter();
+            addTodo();
+            setId(todoId + 1);
+            setTodo('');
+          }}
+          autoFocus={true}
+          value={todo}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            increaseCounter();
+            addTodo();
+            setId(todoId + 1);
+            setTodo('');
+          }}>
+          <Text style={styles.btnText}>ADD TODO</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
+    flex: 1,
+    backgroundColor: '#37474F',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: 15,
+    marginVertical: 10,
+  },
+  heading: {
+    color: '#FBA730',
+    fontWeight: 'bold',
+    fontSize: 35,
+  },
+  counter: {
+    color: '#FBA730',
+    fontSize: 30,
+  },
 
-  }
-})
+  main: {
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  footer: {
+    backgroundColor: '#B0BEC5',
+    marginHorizontal: 10,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: Dimensions.get('window').height / 5,
+    marginBottom: 25,
+  },
+  input: {
+    backgroundColor: '#FFFFFF',
+    width: '85%',
+    height: '32%',
+    borderRadius: 10,
+    marginVertical: 10,
+  },
+  button: {
+    backgroundColor: '#37474F',
+    width: '50%',
+    height: '30%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  btnText: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
+});
 
 export default App;
