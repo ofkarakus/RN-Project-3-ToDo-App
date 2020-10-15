4b508ea558a924d51756d2815ed9d13892a70d6d
ofkarakus <omerfarukkarakus01@gmail.com>
2020-10-15 09:01:05 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+const App = () => {
+  
+  
+  return (
+    <SafeAreaView style={styles.container}>
+
+
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+
+  }
+})
+
+export default App;
